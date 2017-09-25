08e69f5c46e1d8eaa68cdd2b3e28d08b0028d0ca
Andrey Reznik <andrey.reznik@yahoo.com>
2017-09-25 16:44:15 +0300

    New commit

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ def ban_new_chat_members(message):
     new_chat_members = message.new_chat_members
     chat = message.chat
     for new_member in new_chat_members:
-        bot.restrict_chat_member(chat.id, new_member.id, can_send_messages=False)
-        print('Заблокирован пользователь: {}'.format(new_member.first_name))
+        print(new_member)
+        if new_member.username != 'styr2245':
+            try:
+                bot.restrict_chat_member(chat.id, new_member.id, can_send_messages=False)
+                print('Заблокирован пользователь: {}'.format(new_member.first_name))
+            except Exception:
+                pass
     bot.delete_message(chat.id, message.message_id)
 
 @bot.message_handler(content_types=['text'])
 def ban_spam_member(message):
-    if message.from_user.username != 'andrey_reznik':
+    if message.from_user.username != 'styr2245':
         chat_member = message.from_user
         chat = message.chat
-        bot.restrict_chat_member(chat.id, chat_member.id, can_send_messages=False)
-        print('Заблокирован пользователь: {}'.format(chat_member.first_name))
+        try:
+            bot.restrict_chat_member(chat.id, chat_member.id, can_send_messages=False)
+            print('Заблокирован пользователь: {}'.format(chat_member.first_name))
+        except Exception:
+            pass
         bot.delete_message(chat.id, message.message_id)
 
 bot.polling()
