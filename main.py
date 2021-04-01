fc3b405292c326ccdbf2dce143b0c1278941edbe
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-01 11:41:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from asyncio import sleep
 
 import telebot
 
-bot = telebot.TeleBot('1673894668:AAFGDY8zeBKDFhGs2jUzZBL8-NySaoy204g')
+bot = telebot.TeleBot('1753134503:AAHPxfcMLv8ucVMJAoaIFkqn6z4uWvmwXdY')
 
 
 @bot.message_handler(content_types=['cmd_call_admins'])
 def cmd_call_admins(bot, update):
-    users = telebot.get_participants(1001472566181)
+    users = telebot.get_participants(1001326131404)
 print(users[0].first_name)
 
 for user in users:
     if user.username is not None:
         print(user.username)
-        telebot.send_message(1001472566181, "@{}".format(user.username))
+        telebot.send_message(1001326131404, "@{}".format(user.username))
         time.sleep(2)
 
 bot.polling()
